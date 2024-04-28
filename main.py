b96f4d4e77313f99d987fa0dd37de45e20a19902
Cesar <cesarjoelalarconguzman@gmail.com>
2024-04-27 21:56:41 -0500

    Cambiando valor de variable por valor general

--- main.py
+++ main.py
@@ -1,25 +1,23 @@
 import telebot
 
 #Token obtenido de BotFather
-TOKEN ='7022399311:AAEwibGGtU1rxqUv44KDk6HCVulbovGW5qg' #Remplaza este código
+TOKEN ='AQUI_TOKEN' #Remplaza este código
 
 #Creamos una instancia de bot y conección a telegram
 bot = telebot.TeleBot(TOKEN)
-
-
-
-#Crear comando '/roadmap'
-@bot.message_handler(commands=['roadmap'])
-
-def envia_roadmap(message):
-    bot.reply_to(message, 'Mensaje de roadmap')
-
 
 #Crear comando '/help'
 @bot.message_handler(commands=['help'])
 
 def enviar_help(message):
-    bot.reply_to(message, 'Comandos: /n/roadmap = Muesta la ruta de aprendizaje que seguiremos en el grupo')
+    bot.reply_to(message, 'Mensaje de comando') #Remplaza este mensaje
+
+#Crear comando '/roadmap'
+@bot.message_handler(commands=['roadmap'])
+
+def envia_roadmap(message):
+    bot.reply_to(message, 'Mensaje de comando') #Remplaza este mensaje
+
 
 
 #Iniciamos el bot
